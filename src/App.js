71da3c4373377ef323eed52bb05cad2fd79529fa
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-08-17 14:24:15 +0200

    css menu start ok; test icone corbeille ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import windowslogo from "./kisspng-windows-xp-logo-microsoft-windows-1-0-windows-logos-5ab719fdd15b24.0641833615219491818575.png";
+import corbeille from "./kisspng-trash-computer-icons-windows-xp-recycling-computer-bin-5b17489fd13dd1.5815833615282525758571.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>xp style portfolio</h1>
-      <img src="https://images2.alphacoders.com/941/thumb-1920-941898.jpg"></img>
+      <div className="bg">
+        <div className="corcontainer">
+          <img className="corbeille" src={corbeille}></img>
+          <p>Corbeille</p>
+        </div>
+      </div>
+
+      <footer className="startmenu">
+        <div className="dem">
+          <img alt="logowindows" className="logostart" src={windowslogo}></img>
+          <p className="demarrer">démarrer</p>
+        </div>
+      </footer>
     </div>
   );
 }
