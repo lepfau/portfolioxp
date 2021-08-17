f6583c878a13330b3663acfe8b1823e321e8a246
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-08-17 12:29:19 +0200

    update test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>xp style portfolio</h1>
       <img src="https://images2.alphacoders.com/941/thumb-1920-941898.jpg"></img>
     </div>
   );
