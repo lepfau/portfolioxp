d32b30b8efb1cf0a2662d000728ce9b771fddfe4
lepfau <edouard.pfauwadel@gmail.com>
2021-09-13 16:35:54 +0200

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main";
 
 function App() {
   return (
+    <HashRouter basename='/'>
     <div className="App">
-      
-      <Main />
-    
+    <Route exact path="/" component={Main} />
+   
+
     </div>
+    </HashRouter>
   );
 }
 
