19b86d5df03e9858dff4bcc7828e413f41556c14
lepfau <edouard.pfauwadel@gmail.com>
2021-09-14 15:30:33 +0200

    last push day

--- src/pages/First.js
+++ src/pages/First.js
@@ -1,16 +1,24 @@
 import React, {useState} from 'react'
 import Main from "./Main";
 import Homepage from "./Homepage";
+import useSound from "use-sound";
+import postbeep from "../assets/postbeep.mp3"
+import Zoom from 'react-reveal/Zoom';
 
 function First() {
+
+    const [play] = useSound(postbeep);
 
     const [loading, setLoading] = useState(false)
     const [xpload, setXpload] = useState(false)
     const [bureau, setBureau] = useState(false)
     const [blink, setBlink] = useState(false)
+    const [arrow, setArrow] = useState(true)
     
     function gifShowing() {
-        setBlink(true)
+        play();
+        setArrow(false);
+        setBlink(true);
         setTimeout(() => {
             setLoading(true);
         
@@ -37,8 +45,8 @@ const [main, setMain] = useState(false)
 
     return (
         <div style={{height:"100%", width:'100%'}}>
-           {home && <Homepage loading={loading} xpload={xpload} bureau={bureau} gifShowing={gifShowing} blink={blink}/>}
-           {main && <Main/>}
+           {home && <Homepage loading={loading} xpload={xpload} bureau={bureau} gifShowing={gifShowing} blink={blink} arroww={arrow}/>}
+           {main &&  <Main/>}
         </div>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,15 +3,17 @@ import photopc from "../assets/pngordi.png"
 import bios from "../assets/bios.gif"
 import xpgif from "../assets/xploading.gif"
 import bureaupng from "../assets/bureau.PNG"
+import arrow from "../assets/arrow.png"
 
 function Homepage(props) {
 
-const {loading, xpload, bureau, blink} = props
+const {loading, xpload, bureau, blink, arroww} = props
 
 
     return (
         <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
             <p className="start-btn" onClick={() => props.gifShowing()}>"X"</p>
+            {blink && <div class="blink_me3">.</div>}
             {blink && <div class="blink_me2">.</div>}
             {blink && <div class="blink_me">.</div>}
             <img className="photopc" src={photopc}></img>
@@ -19,6 +21,9 @@ const {loading, xpload, bureau, blink} = props
             {loading &&  <img className="bios" src={bios}></img>}
             {xpload && <img className="xploading" src={xpgif}></img> }
             {bureau && <img className="bios" src={bureaupng}></img>}
+            {arroww && <p className="indicationdem">Démarrer l'ordinateur</p>}
+            {arroww && <img className="arrow" src={arrow}></img>}
+            
         
         </div>
     )
